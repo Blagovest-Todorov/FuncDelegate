2ad81e59a9c22c2e42552c32dd1d5f73376f4279
Blagovest Todorov <72020361+Blagovest-Todorov@users.noreply.github.com>
2021-04-23 09:31:11 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,11 +14,11 @@ namespace Delegates
         {
             return x + y;
         } // Delegate is variable that points to a method, it is reference type,
-          // Delegate provades a way which tells whick method to be called wehn
+          // Delegate provides a way which tells which method to be called when
           // an event is triggered
           // Delegate provides way to encapsulate the methods
-          // Delegate tis ref type that provide reference to methods with particular parmeter list
-          //and return type and then calls the methis in a program for execution when it si needed
+          // Delegate is ref type that provide reference to methods with particular parmeter list
+          //and return type and then calls the methods in a program for execution when it is needed
           // Delegate may have zero or more input parameters and one out / oputput-return param
     }
 }
